8fde79019a70e115b7d5ab2234efb9b32533e6e0
Lukas Bellaart <99068241@mydavinci.nl>
2021-09-08 09:33:30 +0200

    min en max

--- meten.py
+++ meten.py
@@ -3,9 +3,15 @@ varB = int(input("Wat is b? -> "))
 
 if varA > varB:
     max = str(varA)
+    min = str(varB)
     print("a is het grootste getal: "+ max)
 elif varA < varB:
     min = str(varA)
+    max = str(varB)
     print("a is het kleinste getal: "+min)
 elif varA == varB:
+    min, max = str(varA), str(varB)
     print("a en b zijn even groot")
+
+print('Het minimum is:', min)
+print("Het maximum is:", max)
