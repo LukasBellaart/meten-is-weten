b150c137e55251aad3d6368c49aa0fbbbe0973d2
Lukas Bellaart <99068241@mydavinci.nl>
2021-09-08 09:28:48 +0200

    elif-statement

---
+++ meten.py
@@ -0,0 +1,9 @@
+varA = int(input("Wat is a? -> "))
+varB = int(input("Wat is b? -> "))
+
+if varA > varB:
+    max = str(varA)
+    print("a is het grootste getal: "+ max)
+elif varA < varB:
+    min = str(varA)
+    print("a is het kleinste getal: "+min)
